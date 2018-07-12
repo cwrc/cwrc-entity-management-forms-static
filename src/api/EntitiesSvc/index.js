4743f3b1eb21af6c19ec2eb5a11f4a4e4064d2be
Andrew <ajmacdonald@users.noreply.github.com>
2018-07-12 17:18:58 -0400

    updating api

--- src/api/EntitiesSvc/index.js
+++ src/api/EntitiesSvc/index.js
@@ -2,28 +2,29 @@
 import {get, put, post} from '../utils'
 
 export const getPerson = async (id) => {
-	get(`http://${process.env.HOST}:${process.env.PORT}/${process.env.REACT_APP_ENTITIES_BASE}/person/${id}`)
+	console.log(`trying ${process.env.REACT_APP_ENTITIES_HOST}/${process.env.REACT_APP_ENTITIES_BASE}/person/${id}`)
+	get(`${process.env.REACT_APP_ENTITIES_HOST}/${process.env.REACT_APP_ENTITIES_BASE}/person/${id}`)
 }
 export const postPerson = async (data) =>
-	post(`http://${process.env.HOST}:${process.env.PORT}/${process.env.REACT_APP_ENTITIES_BASE}/person`, data)
+	post(`${process.env.REACT_APP_ENTITIES_HOST}/${process.env.REACT_APP_ENTITIES_BASE}/person`, data)
 
 export const putPerson = async (id, data) =>
-	put(`http://${process.env.HOST}:${process.env.PORT}/${process.env.REACT_APP_ENTITIES_BASE}/person/${id}`, data)
+	put(`${process.env.REACT_APP_ENTITIES_HOST}/${process.env.REACT_APP_ENTITIES_BASE}/person/${id}`, data)
 
 export const getPlace = async (id) =>
-	get(`http://${process.env.HOST}:${process.env.PORT}/${process.env.REACT_APP_ENTITIES_BASE}/place/${id}`)
+	get(`${process.env.REACT_APP_ENTITIES_HOST}/${process.env.REACT_APP_ENTITIES_BASE}/place/${id}`)
 
 export const postPlace = async (data) =>
-	post(`http://${process.env.HOST}:${process.env.PORT}/${process.env.REACT_APP_ENTITIES_BASE}/place`, data)
+	post(`${process.env.REACT_APP_ENTITIES_HOST}/${process.env.REACT_APP_ENTITIES_BASE}/place`, data)
 
 export const putPlace = async (id, data) =>
-	put(`http://${process.env.HOST}:${process.env.PORT}/${process.env.REACT_APP_ENTITIES_BASE}/place/${id}`, data)
+	put(`${process.env.REACT_APP_ENTITIES_HOST}/${process.env.REACT_APP_ENTITIES_BASE}/place/${id}`, data)
 
 export const getOrganization = async (id) =>
-	get(`http://${process.env.HOST}:${process.env.PORT}/${process.env.REACT_APP_ENTITIES_BASE}/organization/${id}`)
+	get(`${process.env.REACT_APP_ENTITIES_HOST}/${process.env.REACT_APP_ENTITIES_BASE}/organization/${id}`)
 
 export const postOrganization = async (data) =>
-	post(`http://${process.env.HOST}:${process.env.PORT}/${process.env.REACT_APP_ENTITIES_BASE}/organization`, data)
+	post(`${process.env.REACT_APP_ENTITIES_HOST}/${process.env.REACT_APP_ENTITIES_BASE}/organization`, data)
 
 export const putOrganization = async (id, data) =>
-	put(`http://${process.env.HOST}:${process.env.PORT}/${process.env.REACT_APP_ENTITIES_BASE}/organization/${id}`, data)
+	put(`${process.env.REACT_APP_ENTITIES_HOST}/${process.env.REACT_APP_ENTITIES_BASE}/organization/${id}`, data)
