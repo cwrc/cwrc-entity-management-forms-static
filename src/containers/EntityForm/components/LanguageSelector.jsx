245204a9a5e7c8270fe8e2a388be15ec9fca8fae
Andrew <ajmacdonald@users.noreply.github.com>
2018-07-19 12:29:52 -0400

    use ISO639-2T where possible

--- src/containers/EntityForm/components/LanguageSelector.jsx
+++ src/containers/EntityForm/components/LanguageSelector.jsx
@@ -3,7 +3,16 @@ import React from 'react'
 import {Field} from 'redux-form'
 import {DropdownComponent} from './FormControls'
 
-import langOptions from '../../../static/lang.js'
+import iso6392 from 'iso-639-2'
+
+const langOptions = iso6392.map(lang => {
+	const isoVal = lang.iso6392T === null ? lang.iso6392B : lang.iso6392T
+	return {
+		text: lang.name,
+		value: isoVal,
+		key: isoVal
+	}
+})
 
 const LanguageSelector = ({
 	input,
