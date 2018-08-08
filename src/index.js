6eddd3c4d130dcf576399c0994082b9ad5e9456e
Andrew <ajmacdonald@users.noreply.github.com>
2018-08-08 19:27:34 -0400

    add promise middleware

--- src/index.js
+++ src/index.js
@@ -5,8 +5,7 @@ import {
 	PageLayout,
 	MainLayout,
 	MainContent,
-	MainContainer,
-	StyledDimmer
+	MainContainer
 } from './containers/App/style'
 
 // necessary?
@@ -19,12 +18,9 @@ import {ThemeProvider} from 'styled-components'
 import theme from './styles/theme'
 
 import {createBrowserHistory} from 'history'
-import {Route} from 'react-router-dom'
 
-import {
-	ConnectedRouter,
-	routerMiddleware
-} from 'react-router-redux'
+import {ConnectedRouter, routerMiddleware} from 'react-router-redux'
+import promiseMiddleware from 'redux-promise-middleware'
 
 import RoutingWrapper from './components/addons/RoutingWrapper'
 import {getRouterRoutes, getRoutes} from './routing'
@@ -37,21 +33,17 @@ import rootReducer from './reducers'
 import Footer from './components/parts/Footer'
 import Header from './components/parts/Header'
 
-import Dashboard from './containers/Dashboard'
-import Person from './containers/EntityForm/Person'
-import Place from './containers/EntityForm/Place'
-import Organization from './containers/EntityForm/Organization'
-
 const initialState: GlobalState = window.__INITIAL_STATE__ || {}
 
 const history = createBrowserHistory()
 
-const middleware = routerMiddleware(history)
+const middlewares = [routerMiddleware(history), promiseMiddleware()]
+const enhancers = middlewares.map(a => applyMiddleware(a))
 
 const store = createStore(
 	rootReducer,
 	initialState,
-	applyMiddleware(middleware)
+	enhancers
 )
 
 console.log('__INITIAL_STATE__:', initialState)
