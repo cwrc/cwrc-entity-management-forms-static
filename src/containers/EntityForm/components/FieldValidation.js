f0a309c6e6c7e40c37d1821cc6a09c3402a445a2
Andrew <ajmacdonald@users.noreply.github.com>
2018-08-27 11:32:03 -0400

    more date range validation

--- src/containers/EntityForm/components/FieldValidation.js
+++ src/containers/EntityForm/components/FieldValidation.js
@@ -62,12 +62,16 @@ export const dateRange = (dateRange: Object) => {
 				const m2 = moment(d2.replace(/-/g, '/'))
 				if (q1 === 'from-iso' && q2 === 'to-iso' && m1.isSameOrAfter(m2)) return {date2: 'To date must occur after From date'}
 				if (q2 === 'from-iso' && q1 === 'to-iso' && m2.isSameOrAfter(m1)) return {date2: 'From date must occur before To date'}
-				if (q1 === 'notBefore-iso' && q2 === 'notAfter-iso' && m1.isAfter(m2)) return {date2: 'Not After date must occur after Not Before date'}
-				if (q2 === 'notBefore-iso' && q1 === 'notAfter-iso' && m2.isAfter(m1)) return {date2: 'Not Before date must occur before Not After date'}
 				if (q1 === 'from-iso' && q2 === 'notAfter-iso' && m1.isAfter(m2)) return {date2: 'Not After date must occur after From date'}
 				if (q2 === 'from-iso' && q1 === 'notAfter-iso' && m2.isAfter(m1)) return {date2: 'From date must occur before Not After date'}
+				if (q1 === 'from-iso' && q2 === 'notBefore-iso' && m1.isBefore(m2)) return {date2: 'Not Before date must occur before From date'}
+				if (q2 === 'from-iso' && q1 === 'notBefore-iso' && m2.isBefore(m1)) return {date2: 'From date must occur after Not Before date'}
+				if (q1 === 'notBefore-iso' && q2 === 'notAfter-iso' && m1.isAfter(m2)) return {date2: 'Not After date must occur after Not Before date'}
+				if (q2 === 'notBefore-iso' && q1 === 'notAfter-iso' && m2.isAfter(m1)) return {date2: 'Not Before date must occur before Not After date'}
 				if (q1 === 'notBefore-iso' && q2 === 'to-iso' && m1.isAfter(m2)) return {date2: 'To date must occur after Not Before date'}
 				if (q2 === 'notBefore-iso' && q1 === 'to-iso' && m2.isAfter(m1)) return {date2: 'Not Before date must occur before To date'}
+				if (q1 === 'notAfter-iso' && q2 === 'to-iso' && m1.isAfter(m2)) return {date2: 'To date must occur before Not After date'}
+				if (q2 === 'notAfter-iso' && q1 === 'to-iso' && m2.isAfter(m1)) return {date2: 'Not After date must occur after To date'}
 			}
 		}
 	}
